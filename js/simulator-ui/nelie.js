4013b2f3da9ca166d09794aaea106452d8217ee0
mesis <mewsyoui@hotmail.com>
2025-02-06 14:20:56 +0900

    テスト更新

---
+++ js/simulator-ui/nelie.js
@@ -0,0 +1,10 @@
+COMMON.promiseI18n.then(() => {
+    
+    var NELIE = Vue.createApp({
+	computed: {
+            onclickNLFleetPresetLoad: function () {
+                console.log("test");
+            }
+        }
+    });
+});
